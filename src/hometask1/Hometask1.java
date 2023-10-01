6539eff4d9ee3cc798a2a738b7f5caca0fc0702b
Ivan Zuev <i9819856754@gmail.com>
2023-10-02 00:41:20 +0300

    The first hometask was finished

---
+++ src/hometask1/Hometask1.java
@@ -0,0 +1,7 @@
+package hometask1;
+
+public class Hometask1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the home task!\n Let's see changes!");
+    }
+}
---
+++ src/hometask2rename/HomeTask2.java
@@ -0,0 +1,7 @@
+package hometask2rename;
+
+public class HomeTask2 {
+    public static void main(String[] args) {
+        System.out.println("Let's play the game!");
+    }
+}
