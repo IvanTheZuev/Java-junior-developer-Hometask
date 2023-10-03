9e1acf6149264801e1c5420a23dd8bf4f9378329
Ivan Zuev <i9819856754@gmail.com>
2023-10-03 21:02:50 +0300

    дз №2

--- src/hometask02/Hometask02.java
+++ src/hometask02/Hometask02.java
@@ -1,2 +1,39 @@
-package hometask02;public class Hometask02 {
+package hometask02;
+
+public class Hometask02 {
+    public static void main(String[] args) {
+
+        //Первое задание
+
+        int numberOfLetters = 20000;
+        int numberOfSendLetters = 17000;
+
+        System.out.println(numberOfLetters - numberOfSendLetters);
+
+        //Второе задание
+
+        var timeKmh = 19;
+        var distanceKm = 2500;
+
+        var velocity = distanceKm * 1000 / (timeKmh * 60 * 60);
+
+        //Третье задание
+
+        int naturalnumber = 98;
+        naturalnumber = 9 + 8;
+        System.out.println(naturalnumber);
+
+        // Четвертое занятие
+
+        long num1 = 1_000_000_000_000_000L;
+        double num2 = -12.875;
+
+        long reverseNum1 = -num1;
+        double reverseNum2 = -num2;
+
+        System.out.println(reverseNum1);
+        System.out.println(reverseNum2);
+
+
+    }
 }
