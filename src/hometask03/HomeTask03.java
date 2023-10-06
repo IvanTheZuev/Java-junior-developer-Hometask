fd463a4f1cb528f930985529331beb9a89a75344
Ivan Zuev <i9819856754@gmail.com>
2023-10-06 14:52:46 +0300

    ДЗ №3 с 4 и 5 задачей

--- src/hometask03/HomeTask03.java
+++ src/hometask03/HomeTask03.java
@@ -64,8 +64,103 @@ public class HomeTask03 {
         //Написать программу, которая в зависимости от значения monthNumberвыводит в консоль количество дней в этом месяце.
         // Необходимо рассмотреть два случая: год не является високосным и год является високосным.
 
-        int monthNumber = 1;
-        boolean isLeap = true;
+        int monthNumber = 12;
+        boolean isLeap = false;
+
+        switch(monthNumber){
+
+            case 1:
+                if (monthNumber == 1)
+                {
+                System.out.println("Январь - 31 день.");
+                }
+            case 2:
+                if (isLeap == true && monthNumber == 2) {
+                    System.out.println("Высокосный год: февраль - 29 дней.");
+                } else System.out.println("Февраль - 28 дней.");
+            case 3:
+                if (monthNumber == 3)
+                {
+                System.out.println("Март - 31 день.");
+                }
+            case 4:
+                if (monthNumber == 4)
+                {
+                    System.out.println("Апрель - 30 дней.");
+                }
+            case 5:
+                if (monthNumber == 5)
+                {
+                System.out.println("Май - 31 день.");
+                }
+            case 6:
+                if (monthNumber == 6)
+                {
+                System.out.println("Июнь - 30 дней");
+                }
+            case 7:
+                if (monthNumber == 7)
+                {
+                System.out.println("Июль - 31 день.");
+                }
+            case 8:
+                if (monthNumber == 8)
+                {
+                System.out.println("Август - 31 день.");
+                }
+            case 9:
+                if (monthNumber == 9)
+                {
+                System.out.println("Сентябрь - 30 дней.");
+                }
+            case 10:
+                if (monthNumber == 10)
+                {
+                System.out.println("Октябрь - 31 день.");
+                }
+            case 11:
+                if (monthNumber == 11)
+                {
+                System.out.println("Ноябрь - 30 дней.");
+                }
+            case 12:
+                if (monthNumber == 12)
+                {
+                System.out.println("Декабрь - 31 день.");
+                }
+        }
+
+
+        //Пятое задание
+        // Даны переменные `couponNumber` - номер купона и `resultSum` - сумма покупки.
+        // 1. Если номер купона - 1111, скидка 10%,
+        // 2. Если номер купона - 3333, скидка 20%,
+        // 3. Если номер купона - 5555, скидка 30%.
+        // 4. В остальных случаях скидка не предусмотрена.
+        //
+        // Необходимо вычислить и сохранить в переменную `resultSum` сумму с учетом скидки, если она предусмотрена.
+        // Вывести значение `resultSum` в консоль.
+
+        int couponNumber = 5555;
+        int resultSum = 15000;
+
+        switch (couponNumber)
+        {
+            case 1111:
+                System.out.println(resultSum - resultSum * 0.1);
+                break;
+            case 3333:
+                System.out.println(resultSum - resultSum * 0.2);
+                break;
+            case 5555:
+                System.out.println(resultSum - resultSum * 0.3);
+                break;
+
+        }
+
+
+
+
 
 
     }
